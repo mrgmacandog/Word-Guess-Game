5718c4c1cd6b802d8def1c3498f1c39a36ee5400
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-03-12 18:39:03 -0700

    Add ability to have non-alphabetic letters to words being guessed and add audio

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,49 +2,65 @@
 
 (function(){
 
-    // Pseudo code
-    /*
-    1. Have a word from an array of word displayed as _ _ _ _ 
-    2. 
+
+
+    // Have a bunch of cards that have pictures and the guesses and then make the appear
+    // functionality for making the game end when all words have been guessed and give stats.
 
 
 
+    const NUM_START_GUESSES = 8;
+    const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 
-    */
-
-    const STARTING_GUESSES = 12;
-
-
-    // Returns the DOM element
+    // Not jQuery but uses same function name to return the DOM element
     function $(id) {
         return document.getElementById(id);
     }
 
     // Hangman game object
     let game = {
-        wins: 0,
-        wordsLeft: ["html", "css", "javascript", "php", "sql"],
+        totalGames: 0, // Number of total games played
+        wins: 0,  // Number of wins
+        wordsLeft: ["m&m's", "reese's", "hershey's", "snickers", "kit kat", "twix", "twizzlers",  
+                    "skittles", "starburst", "milkyway", "butterfinger"],  // Array of candies
         currWord: "",  // Current word
         currWordOutput: "",  // How the word will appear on the webpage
-        currWordLetters: [],  // List of the letters the user had guessed that are not in the word
-        numGuessesLeft: STARTING_GUESSES,  // Amount of guesses the user has left
-        numLettersFound: 0,
-        lettersGuessed: [],
-        startNewGame: function() {  // CHECK
+        currWordLetters: [],  // Nested array with each letter of the word
+                              // and a boolean that signifies if the letter
+                              // should be displayed or not
+        numGuessesLeft: NUM_START_GUESSES,  // Amount of guesses the user has left
+                                            // Starts off with the constant
+        numLettersFound: 0,  // Number of letters found
+        lettersGuessed: "",  // String of letter guessed that are not in the word
+
+        // Starts a new game by selecting a random word from the array of words
+        startNewGame: function() {
             console.log("Wins: " + this.wins);
-            this.currWord = this.wordsLeft[Math.floor(Math.random() * this.wordsLeft.length)];  // CHECK
-            console.log("Word being guessed: " + this.currWord);  // CHECK
+
+            // Selects the random word from the array and sets it as the current word
+            this.currWord = this.wordsLeft[Math.floor(Math.random() * this.wordsLeft.length)];
+            console.log("Word being guessed: " + this.currWord);
 
             // Remove current word from the list of words left
-            this.wordsLeft.splice(this.wordsLeft.indexOf(this.currWord), 1);  // CHECK
+            this.wordsLeft.splice(this.wordsLeft.indexOf(this.currWord), 1);
 
-            for (let i = 0; i < this.currWord.length; i++) {  // CHECK
-                let oneLetter = [this.currWord.charAt(i), false];  // CHECK
-                this.currWordLetters.push(oneLetter);  // CHECK
+
+            for (let i = 0; i < this.currWord.length; i++) {
+                let thisLetter = this.currWord.charAt(i);
+                if (ALPHABET.includes(thisLetter)) {
+                    let oneLetter = [this.currWord.charAt(i), false];
+                    this.currWordLetters.push(oneLetter);
+                } else {
+                    let oneLetter = [this.currWord.charAt(i), true];
+                    this.currWordLetters.push(oneLetter);
+                    this.numLettersFound++;
+                }
+                
             }
 
             this.createOutput();
 
+            $("total-games").textContent = this.totalGames;
             $("num-wins").textContent = this.wins;
             $("curr-word").textContent = this.currWordOutput;
             $("num-guesses-left").textContent = this.numGuessesLeft;
@@ -52,13 +68,14 @@
 
 
         },
-        guessLetter: function(key) {  // CHECK
+        guessLetter: function(key) {
             if (!this.lettersGuessed.includes(key)) {
                 if (this.currWord.includes(key)) {
                     this.updateOutput(key);
                 } else {  // Add to lettersGuessed
                     console.log("Word doesn't include " + key);
-                    this.lettersGuessed.push(key);
+                    // this.lettersGuessed.push(key);
+                    this.lettersGuessed += key + "\xa0";
                     this.numGuessesLeft--;  // Add to lettersGuessed
                 }
                 
@@ -69,6 +86,10 @@
             console.log("Current word output: " + this.currWordOutput);
             $("curr-word").textContent = this.currWordOutput;
             $("num-guesses-left").textContent = this.numGuessesLeft;
+
+
+
+
             $("letters-guessed").textContent = this.lettersGuessed;
 
         },
@@ -76,9 +97,9 @@
             this.currWordOutput = "";
             for (let i = 0; i < this.currWordLetters.length; i++) {
                 if (this.currWordLetters[i][1] === true) {
-                    this.currWordOutput += this.currWordLetters[i][0];
+                    this.currWordOutput += "\xa0" + this.currWordLetters[i][0] + "\xa0";
                 } else {
-                    this.currWordOutput += " _ ";
+                    this.currWordOutput += "\xa0_\xa0";
                 }
             }
             
@@ -93,9 +114,9 @@
                     this.currWordLetters[i][1] = true;
                     this.numLettersFound++;
                 }
-                
             }
             console.log("# letters found: " + this.numLettersFound);
+            console.log("current word length: " + this.currWord.length);
 
             this.createOutput();
         },
@@ -105,22 +126,17 @@
         reset: function() {
             this.currWordOutput = "";
             this.currWordLetters = [];
-            this.numGuessesLeft = STARTING_GUESSES;
+            this.numGuessesLeft = NUM_START_GUESSES;
             this.numLettersFound = 0;
             this.lettersGuessed = [];
         }
     }
 
-
-
-
     window.onload = function() {
         // Testing area
         
-        // game.displayWord();
-
         // Initiate a new game
-        game.startNewGame();  // CHECK
+        game.startNewGame();
 
         // Listens for a key press
         document.onkeyup = function(event) {
@@ -128,7 +144,7 @@
 
             if (event.keyCode >= 65 && event.keyCode <= 90) {
                 // Guesses with a key
-                game.guessLetter(event.key);  // CHECK
+                game.guessLetter(event.key); 
 
                 // Continue with current word if it's not solved yet
                 if (!game.checkIfSolved() && game.numGuessesLeft > 0) {
@@ -138,10 +154,19 @@
                     if (game.checkIfSolved()) {
                         console.log("You solved it!");
                         game.wins++;
+
+                        // Open assets/audio/Fanfare-sound/Read.txt for licencing
+                        let audio = new Audio("assets/audio/Fanfare-sound/Fanfare-sound.mp3");
+                        audio.play();
                     } else {
                         console.log("You ran out of guesses. Game over!");
+
+                        // Open assets/audio/womp-womp/Read.txt for licencing
+                        let audio = new Audio("assets/audio/womp-womp/womp-womp.mp3");
+                        audio.play();
                     }
-                    
+                    game.totalGames++;
+                    console.log("Total Games:" + game.totalGames)
                     console.log("");
                     game.reset();
                     game.startNewGame();
